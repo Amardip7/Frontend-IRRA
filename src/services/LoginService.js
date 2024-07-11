a70907112372b428ce5404171add52b4a5acb19d
Krishna Kashyap <krishnakashyap0704@gmail.com>
2024-07-11 11:23:22 +0530

    opuser/superuser Addition

--- src/services/LoginService.js
+++ src/services/LoginService.js
@@ -1,24 +1,11 @@
-// import axios from "axios";
-// import {PORT_No, BASE_URL} from "../services/ApiConstant";
-
-// export async function logindata(formData){
-//     try{
-//         const response =await axios.post(`${BASE_URL}${PORT_No}/login`, formData);
-//         console.log(response);
-//     }catch(error){
-//         console.log(error);
-//     } 
-// };
-
 import axios from "axios";
 import { PORT_No, BASE_URL } from "../services/ApiConstant";
 
 export async function logindata(formData) {
     try {
         const response = await axios.post(`${BASE_URL}${PORT_No}/login`, formData);
-        return response.data; // Return the response data
+        return response.data; 
     } catch (error) {
         console.log(error);
-        throw error; // Throw the error to be caught in the calling function
     }
 }
